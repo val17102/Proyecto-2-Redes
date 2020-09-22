48dd97a30a28b0722bbc1668a73cd1f277b87469
val17102 <val17102@uvg.edu.gt>
2020-09-22 11:56:25 -0600

    Funcionalidad de register

--- Proyecto2.py
+++ Proyecto2.py
@@ -4,12 +4,44 @@ import getpass
 from optparse import OptionParser
 
 import sleekxmpp
-
+from sleekxmpp.exceptions import IqError, IqTimeout
 if sys.version_info < (3, 0):
     from sleekxmpp.util.misc_ops import setdefaultencoding
     setdefaultencoding('utf8')
 else:
     raw_input = input
+
+class Register(sleekxmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("register", self.register)
+    
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        # We're only concerned about registering, so nothing more to do here.
+        self.disconnect()
+    
+    def register(self, iq):
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            resp.send(now=True)
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" % 
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
+
 
 class Chat(sleekxmpp.ClientXMPP):
     def __init__(self, jid, password):
@@ -153,7 +185,19 @@ if __name__ == '__main__':
             else:
                 print("Unable to connect.")
         elif (option1 == "2"):
-            print('')
+            opts.jid = raw_input("Username: ")
+            opts.password = getpass.getpass("Password: ")
+
+            xmpp = Register(opts.jid, opts.password)
+            xmpp.register_plugin('xep_0030') # Service Discovery
+            xmpp.register_plugin('xep_0004') # Data forms
+            xmpp.register_plugin('xep_0066') # Out-of-band Data
+            xmpp.register_plugin('xep_0077') # In-band Registration
+            if xmpp.connect():
+                xmpp.process(block=True)
+                print("Done")
+            else:
+                print("Unable to connect.")
         elif (option1 == "3"):
             print('')
         else:
